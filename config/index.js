dc7f174e3b63347526fc2a949c7664826f88745a
Dallas Lones <dallasrlones@gmail.com>
2019-05-27 00:51:30 -0600

    updated oanda fetchYearsTickData

--- config/index.js
+++ config/index.js
@@ -3,4 +3,8 @@
   config.apiKey_CoinBase = '';
   config.apiSecret_CoinBase = '';
 
+  config.baseOandaUrl = '';
+  config.apiKey_Oanda = '';
+  config.apiSecret_Oanda = '';
+
 })(module.exports);
--- services/traderMachine/oandaClient.js
+++ services/traderMachine/oandaClient.js
@@ -1,41 +1,79 @@
-((oandaClient, axios) => {
+((oandaClient, axios, { baseOandaUrl }) => {
 
-  const oandaAPI_URL = '';
+  const client = axios.create({
+    baseURL: baseOandaUrl,
+    timeout: 2000,
+    headers: {}
+  });
+
+  function handleError(methodName, err) {
+    console.log(`${'oandaClient'.yellow} - ${methodName.toString().green} - ${err.toString().red}`);
+    console.log(err);
+  }
 
   oandaClient.fetchAccount = () => {
-    return axios.get(`${oandaAPI_URL}/`);
+    return client.get(`/`);
   };
 
   oandaClient.fetchBalance = () => {
-    return axios.get(`${oandaAPI_URL}/`);
+    return client.get(`/`);
   };
 
   oandaClient.fetchCurrentBuyPrice = () => {
-    return axios.get(`${oandaAPI_URL}/`);
+    return client.get(`/`);
   };
 
   oandaClient.fetchCurrentSellPrice = () => {
-    return axios.get(`${oandaAPI_URL}/`);
+    return client.get(`/`);
   };
 
   oandaClient.fetchCurrentOrders = () => {
-    return axios.get(`${oandaAPI_URL}/`);
+    return client.get(`/`);
   };
 
-  oandaClient.fetchYearsTickData = () => {
-    return axios.get(`${oandaAPI_URL}/`);
+  oandaClient.fetchTickDataFrom = (currencyPair, fromDate) => {
+    return client.get(`/instruments/${currencyPair}/candles?from=${fromDate}`);
+  };
+
+  oandaClient.fetchYearsTickData = (currencyPair) => {
+    const secondsInAYear = (60 * 60 * 24 * 366);
+    const rightNow = Date.now();
+
+    let itterationsNeeded = secondsInAYear / 5000;
+    const fullDataSet = [];
+
+    return new Promise(( resolve, reject ) => {
+      let count = Number(itterationsNeeded);
+
+      for (var i = 0; i <= itterationsNeeded; i++) {
+        oandaClient.fetchTickDataFrom(currencyPair, new Date(rightNow).setSeconds((i + 1) * 5000))
+          .then(({ data }) => {
+            fullDataSet.concat(data.candles);
+            count -= 1;
+
+            if (count === 0) {
+              resolve(fullDataSet);
+            }
+          }).catch((err) => {
+            handleError('fetchYearsTickData', err);
+          });
+      }
+
+    });
   };
 
   oandaClient.buy = ammount => {
-    return axios.post(`${oandaAPI_URL}/`);
+    return client.post(`/`);
   };
 
   oandaClient.sell = ammount => {
-    return axios.post(`${oandaAPI_URL}/`);
+    return client.post(`/`);
   };
 
 })
 (
   module.exports,
   require('axios'),
+  require('../../config'),
+  require('colors')
 );
