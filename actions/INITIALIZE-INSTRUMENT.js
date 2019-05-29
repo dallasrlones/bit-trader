3e045870e21ddceb26137dafe96a358c3bacf960
Dallas Lones <dallasrlones@gmail.com>
2019-05-28 21:28:50 -0600

    messing with initializer

--- actions/INITIALIZE-INSTRUMENT.js
+++ actions/INITIALIZE-INSTRUMENT.js
@@ -10,14 +10,21 @@
     const { addToActionQueue } = actionMachine;
 
     try {
-      const fifteenMinutesInSeconds = (60 * 15);
-      const itterations = Math.ceil(fifteenMinutesInSeconds / 5 / 5000);
+      // 900 seconds
+      const startDate = (60 * 15);
+      // start date divided by 5 second intervals devided by 5k max limit on API call
+      const itterations = Math.ceil(startDate / 5 / 5000);
 
       // friendlyAlert(` SETTING - ${name} - COUNT `);
       setState(`INITIALIZING-${name}-COUNT`, itterations);
 
-      for (var i = 0; i < itterations; i++) {
-        const fromDate = moment().subtract((i + 1) * itterations, 'seconds').unix();
+      if (itterations > 1) {
+        for (var i = 0; i <= itterations; i++) {
+          const fromDate = moment().subtract((i + 1) * itterations, 'seconds').unix();
+          addToActionQueue('INSTANT', { name: 'INITIALIZE-INSTRUMENT-SHARD', params: { name, fromDate }, hasAjax: true });
+        }
+      } else {
+        const fromDate = moment().subtract(startDate, 'seconds').unix();
         addToActionQueue('INSTANT', { name: 'INITIALIZE-INSTRUMENT-SHARD', params: { name, fromDate }, hasAjax: true });
       }
       done();
