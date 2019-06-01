d124fcb5d9bd577574290827dc4fe21b613dbcc1
Dallas Lones <dallasrlones@gmail.com>
2019-06-01 02:36:03 -0600

    market closes on the weekends???? wtf

--- queues/instantQueue.js
+++ queues/instantQueue.js
@@ -1,11 +1,47 @@
-((instantQueue, { stateMachine, actionMachine, utils, playSound }) => {
-  const { getState } = stateMachine;
+((instantQueue, { stateMachine, actionMachine, utils, playSoundInstant }) => {
+  const { getState, setState } = stateMachine;
   const { runActionQueue, addToActionQueue } = actionMachine;
 
   let instantQueueLoop = false;
+  let tradingOpenPlayed = false;
+
+  function dayIs(dayNum) {
+    return (new Date(
+      new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
+    ).getDay() === dayNum)
+  }
+
+  function timePassedClose(){
+    return (new Date(
+      new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
+    ).getHours() >= 15);
+  }
+
+  function isTradingOpen() {
+    if (dayIs(0) || (dayIs(5) ) || dayIs(6)) {
+      if(tradingOpenPlayed === false) {
+        tradingOpenPlayed = true;
+        setState('MARKET-IS-OPEN', false);
+
+        setTimeout(() => {
+          playSoundInstant('market_closed.mp3');
+        }, 1000 * 30);
+      }
+    } else {
+      if (tradingOpenPlayed === true) {
+        tradingOpenPlayed = false;
+        setState('MARKET-IS-OPEN', true);
+
+        setTimeout(() => {
+          playSoundInstant('market_open.mp3');
+        }, 1000 * 30)
+      }
+    }
+  }
 
   instantQueue.startInstantQueue = (instantQueueLoopIntervalSpeed) => {
     instantQueueLoop = setInterval(() => {
+      isTradingOpen();
       if (getState('OANDA-ACCOUNT-PRIMARY') !== undefined) {
         addToActionQueue('INSTANT', { name: 'CHECK-FOR-PROFIT-LOSS' });
 
